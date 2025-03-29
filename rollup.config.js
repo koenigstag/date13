b0eff8dfc289cde96e1992fe3c2af8b395f0e0be
koenigstag <50256886+koenigstag@users.noreply.github.com>
2025-03-29 06:21:43 +0200

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/date13.js',
+      format: 'iife',
+      name: 'Date13',
+      sourcemap: true,
+      plugins: [terser()]
+    },
+    {
+      file: 'dist/date13.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/date13.cjs',
+      format: 'cjs',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ]
+};
